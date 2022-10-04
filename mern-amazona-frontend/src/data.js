a50ba5f538cfc39bea22b6a2b23348c21c44d742
debdevs <debdevs97@gmail.com>
2022-10-04 03:46:28 -0400

    add images and basic products list div

---
+++ mern-amazona-frontend/src/data.js
@@ -0,0 +1,43 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim Shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Adidas Fit Shirt',
+      slug: 'adidas-fit-shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 65,
+      countInStock: 5,
+      brand: 'Nike',
+      rating: 3.5,
+      numReviews: 10,
+      description: 'high quality pants',
+    },
+
+    {
+      name: 'Nike Slim Shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 65,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+  ],
+};
+
+export default data;
